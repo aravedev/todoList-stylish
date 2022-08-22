b6bf2601031e27f9a8d9ea49bb70e5d7fca25506
aravedev <dravedev@gmail.com>
2022-08-22 19:27:24 +0200

    Added functionality to Form and creating objects for each new task

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,36 +1,91 @@
 import React from "react";
+import { useState } from "react";
 import { BiEdit } from "react-icons/bi";
-import { AiFillStar } from "react-icons/ai";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { useEffect } from "react";
 
 export default function Form() {
+  const [task, setTask] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [inputTextArea, setInputTextArea] = useState("");
+  const [starImportant, setStartImportant] = useState(false);
+  const [category, setCategory] = useState("");
+
+  const starImage = starImportant ? (
+    <AiFillStar></AiFillStar>
+  ) : (
+    <AiOutlineStar></AiOutlineStar>
+  );
+
+  const handleStarImportantIcon = () => {
+    setStartImportant(!starImportant);
+  };
+
+  const handleInputTextArea = (event) => {
+    let textArea = event.target.value;
+    setInputTextArea(textArea);
+  };
+
+  const handleInputText = (event) => {
+    let inputText = event.target.value;
+    setInputText(inputText);
+  };
+
+  const handleCategory = () => {
+    const nameCategory = event.target.value;
+    setCategory(nameCategory);
+  };
+
+  useEffect(() => {
+    () => handleAddNewTask();
+  }, []);
+
+  const handleAddNewTask = () => {
+    const obj = {
+      title: inputText,
+      description: inputTextArea,
+      category: category,
+      important: starImportant,
+    };
+
+    setTask((prevState) => [obj, ...prevState]);
+  };
+
+  console.log(task);
+
   return (
     <div className="w-96 h-90 border p-6 bg-backgroundDarkBlue text-white font-light rounded-lg opacity-90 snap-center shadow-2xl flex flex-col">
       <div className=" flex justify-around items-baseline mb-2">
         <span className="font-semibold ">Mark as Important</span>
-        <div className="border w-8 h-8 flex justify-center items-center rounded-full ">
-          <a href="#">
-            <AiFillStar />
+        <div className="border w-8 h-8 flex justify-center items-center rounded-full text-yellow-600">
+          <a href="#" onClick={handleStarImportantIcon}>
+            {starImage}
           </a>
         </div>
       </div>
 
       <div className="mb-4 flex flex-col items-start justify-center  p-2">
         <input
+          onChange={handleInputText}
           type="text"
           placeholder=" Add Title"
-          className="mb-2 w-3/4 rounded-lg py-1 "
+          className="mb-2 w-3/4 rounded-lg py-1 text-black font-semibold px-5 "
         />
         <textarea
-          name=""
-          id=""
-          cols="43"
+          onChange={handleInputTextArea}
+          cols="36"
           rows="5"
-          className="rounded-lg "
+          className="rounded-lg text-black font-semibold px-2"
           placeholder=" Add Description..."
         ></textarea>
         <div className="w-full  p-2 flex justify-between">
           <label htmlFor="category">Category:</label>
-          <select name="category" id="" className="bg-backgroundDarkBlue">
+          <select
+            name="category"
+            id=""
+            className="bg-backgroundDarkBlue"
+            onChange={handleCategory}
+          >
             <option value="hobbies">Hobbies</option>
             <option value="grocery">Grocery</option>
             <option value="work">Work</option>
@@ -40,7 +95,7 @@ export default function Form() {
       </div>
       <div className=" grid  gap-4 place-items-end ">
         <div className="bg-yellow-50 w-12 h-12 rounded-[25px] px-4 py-2 font-bold text-buttonOrange hover:bg-buttonOrange hover:text-white mb-3 flex justify-center items-center">
-          <a href="#">
+          <a href="#" onClick={handleAddNewTask}>
             <BiEdit className="w-8 h-8"></BiEdit>
           </a>
         </div>
